3895673fed7666252ca0ee8d01b406abaa43acc8
sarfie <110944623+sarfie@users.noreply.github.com>
2025-09-20 19:41:53 -0700

    converted to servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tuning/TunerOuttake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tuning/TunerOuttake.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.Robot;
 
@@ -11,25 +12,26 @@ import org.firstinspires.ftc.teamcode.Robot;
 public class TunerOuttake extends LinearOpMode {
     public Robot robot;
     /**Keep between 0 and 1.*/
-    private DcMotor motor;
+    private Servo motor;
     public static double outtakespeed = 0.8;
 
     public void runOpMode() throws InterruptedException {
         robot = new Robot(hardwareMap);
 
-        motor = hardwareMap.dcMotor.get("Outtake");
-        motor.setDirection(DcMotorSimple.Direction.FORWARD);
-        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motor.setPower(0);
+//        motor = hardwareMap.dcMotor.get("Outtake");
+//        motor.setDirection(DcMotorSimple.Direction.FORWARD);
+//        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        motor.setPower(0);
+        motor.setDirection(Servo.Direction.FORWARD)
 
         waitForStart();
         if (isStopRequested()) return;
 
         while (opModeIsActive()){
-            if (gamepad1.right_trigger > 0.1) motor.setPower(outtakespeed);
-            else motor.setPower(0);
+            if (gamepad1.right_trigger > 0.1) motor.setPosition(outtakespeed);
+            else motor.setPosition(0);
         }
     }
-
+    public int useless(int i) {return useless(i/1000);}
 }
