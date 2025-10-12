476a8737e21f4aab6e09b5e78f405c2d3bcc4ad0
sarfie <110944623+sarfie@users.noreply.github.com>
2025-10-11 19:57:35 -0700

    Moved a bunch of stuff to Outtake, movement should no longer be bound to the tuner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tuning/OuttakePIDTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tuning/OuttakePIDTuner.java
@@ -32,8 +32,6 @@ public class OuttakePIDTuner extends LinearOpMode {
 
     Telemetry tele = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-
-
     private double prevPosTop, prevPosBottom, currPosTop, currPosBottom;
     private double startTime = System.nanoTime() / 1e9;
     private PIDController RPMControllerTop = new PIDController(KPTop, 0, 0);
@@ -93,7 +91,6 @@ public class OuttakePIDTuner extends LinearOpMode {
     }
     public void pidtunedmotor(Telemetry telemetry) {
 
-
         prevPosTop = currPosTop;
         prevPosBottom = currPosBottom;
 
@@ -119,12 +116,30 @@ public class OuttakePIDTuner extends LinearOpMode {
         if (topRecords.size() > queueSize)
             topRecords.remove(0);
 
-        bottomRecords.add(currRPMTop);
+        bottomRecords.add(currRPMBottom);
         if (bottomRecords.size() > queueSize)
             bottomRecords.remove(0);
 
-        double averageTop = topRecords.size() >= queueSize ? (topRecords.get(0)+topRecords.get(1)+topRecords.get(2)+topRecords.get(3)+topRecords.get(4))/5 : currRPMTop;
-        double averageBottom = bottomRecords.size() >= queueSize ? (bottomRecords.get(0)+bottomRecords.get(1)+bottomRecords.get(2)+bottomRecords.get(3)+bottomRecords.get(4))/5 : currRPMBottom;
+        double undividedAverageBottom = 0;
+        double undividedAverageTop = 0;
+
+        for (int i = 0; i < topRecords.size(); i++) {
+            undividedAverageTop += topRecords.get(i);
+            undividedAverageBottom += topRecords.get(i);
+        }
+        double averageTop;
+        double averageBottom;
+        if (undividedAverageTop > 0) {
+            averageTop = undividedAverageTop / queueSize;
+            averageBottom = undividedAverageBottom / queueSize;
+        } else {
+            averageTop = currRPMTop;
+            averageBottom = currRPMBottom;
+        }
+
+        //double averageTop = topRecords.size() >= queueSize ? (topRecords.get(0)+topRecords.get(1)+topRecords.get(2)+topRecords.get(3)+topRecords.get(4))/5 : currRPMTop;
+        //double averageBottom = bottomRecords.size() >= queueSize ? (bottomRecords.get(0)+bottomRecords.get(1)+bottomRecords.get(2)+bottomRecords.get(3)+bottomRecords.get(4))/5 : currRPMBottom;
+
         telemetry.addData("ready", bottomRecords.size() >= queueSize);
         telemetry.addData("top", averageTop);
         telemetry.addData("bottom", averageBottom);
