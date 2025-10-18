7b1d0b3a03edce0cceae1a4aa5442485c41fff6f
sarfie <110944623+sarfie@users.noreply.github.com>
2025-10-18 09:28:23 -0700

    Fixed Easy3Points

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousScripts/Easy3Points.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousScripts/Easy3Points.java
@@ -1,20 +1,27 @@
 package org.firstinspires.ftc.teamcode.Autonomous.AutonomousScripts;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.Modules.Drivetrain;
 
+@Autonomous(name="Meet0")
 public class Easy3Points extends LinearOpMode {
     Drivetrain drivetrain;
 
+
     @Override
     public void runOpMode() throws InterruptedException {
-        drivetrain.startOdometry();
+
+        drivetrain = new Drivetrain(hardwareMap);
 
         waitForStart();
 
         while (opModeIsActive()) {
             drivetrain.setWheelPowers(0.25,0.25,0.25,0.25);
+            Thread.sleep(1000);
+            drivetrain.setWheelPowers(0,0,0,0);
+            break;
         }
     }
 }
