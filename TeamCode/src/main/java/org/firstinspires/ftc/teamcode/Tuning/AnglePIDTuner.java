d52af6e34a9563daf05cae087fcade95255c26bc
MrWalium <coolcoderai123@gmail.com>
2025-11-01 07:49:42 -0700

    fixed logic for the error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tuning/AnglePIDTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tuning/AnglePIDTuner.java
@@ -10,9 +10,6 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.Modules.PIDController;
 import org.firstinspires.ftc.teamcode.Robot;
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
-
-import java.util.ArrayList;
 
 
 @Config
@@ -21,24 +18,23 @@ public class AnglePIDTuner extends LinearOpMode {
 
     Robot robot;
     PIDController anglePID;
+    private double initHeading = 0;
 
     public static boolean align = false;
     boolean alignDebounce = false;
     public static double kp = 0, ki = 0, kd = 0;
+
+    public static double goal = 0;
+    private double prevGoal = 0;
     public static double error = 0;
     private Telemetry t = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-    String goalTag = "Red Goal";
 
     @Override
     public void runOpMode() throws InterruptedException {
         robot = new Robot(hardwareMap);
-
         boolean prevAlign = false;
 
         anglePID = new PIDController(kp, ki, kd);
-
-        double prevHeading = 0;
-
 
         waitForStart();
         if (isStopRequested()) return;
@@ -50,7 +46,7 @@ public class AnglePIDTuner extends LinearOpMode {
             }
 
             if(align && !alignDebounce) {
-                prevHeading = robot.getDrivetrain().getRobotHeading(AngleUnit.DEGREES) % 360;
+                initHeading = currHeading() % 360;
             }
 
             if (gamepad1.triangle) {
@@ -59,7 +55,7 @@ public class AnglePIDTuner extends LinearOpMode {
             }
 
             if(gamepad1.circle) {
-                prevHeading = robot.getDrivetrain().getRobotHeading(AngleUnit.DEGREES) % 360;
+                initHeading = currHeading() % 360;
             }
 
             if(prevAlign != align) {
@@ -67,39 +63,42 @@ public class AnglePIDTuner extends LinearOpMode {
                 robot.getDrivetrain().resetImu();
             }
 
-            fieldCentricDrive(prevHeading);
+            if(prevGoal != goal) {
+                initHeading = currHeading();
+                anglePID.setPID(kp, ki, kd);
+            }
+
+            fieldCentricDrive();
             prevAlign = align;
+            prevGoal = goal;
         }
     }
 
-    public double turnPowerAngle(double angle) {
-        //AprilTagDetection goalTag = robot.getCamera().getGoalApriltag();
-
-        //if (goalTag == null) {return 0; }
-
-        return anglePID.calculate(0, angle);
+    public double currHeading() {
+        return robot.getDrivetrain().getRobotHeading(AngleUnit.DEGREES);
     }
 
-    private void fieldCentricDrive(double prevHeading) {
+    private void fieldCentricDrive() {
         double slowdown = gamepad1.right_trigger > 0 ? 0.25 : 1;
         double y = -gamepad1.left_stick_y * slowdown;
         double x = gamepad1.left_stick_x * 1.1 * slowdown;
-        error = prevHeading - robot.getDrivetrain().getRobotHeading(AngleUnit.DEGREES);
-        error %= 360;
-        double alignVal = turnPowerAngle(error);
-        if(alignVal == 0 || error <= 2) { align = false; alignDebounce = false;}
-        t.addData("Current Heading", robot.getDrivetrain().getRobotHeading(AngleUnit.DEGREES));
-        t.addData("Previous Heading", prevHeading);
+
+        double currHeading = currHeading();
+        double goalHeading = initHeading*2 - currHeading();
+        error = goalHeading - currHeading;
+        if(Math.abs(error) > 180) { error = (error + -Math.signum(error) * 180); }
+        double alignVal = anglePID.calculate(0, error);
+        if(alignVal == 0 || Math.abs(error) <= 2) { align = false; alignDebounce = false;}
+        t.addData("Current Heading", currHeading);
+        t.addData("Initial Heading", initHeading);
         t.addData("Turn Power", alignVal);
         t.addData("Error", error);
         t.update();
 
         double rx = align ? alignVal : gamepad1.right_stick_x * slowdown;
 
-        double heading = robot.getDrivetrain().getRobotHeading(AngleUnit.RADIANS);
-
-        double rotX = x * Math.cos(-heading) - y * Math.sin(-heading);
-        double rotY = x * Math.sin(-heading) + y * Math.cos(-heading);
+        double rotX = x * Math.cos(-currHeading) - y * Math.sin(-currHeading);
+        double rotY = x * Math.sin(-currHeading) + y * Math.cos(-currHeading);
 
         double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
         double frWheelPower = (rotY - rotX - rx) / denominator;
