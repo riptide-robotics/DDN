cb9d2488cff4d0cd05f6894a6f33127cb74c4202
TreeSquirrles <herkyaaron@gmail.com>
2025-11-16 12:38:19 -0800

    Tank Drive Movement Class meant to act as a tester for the soon to come LinearTrajectoryBuilder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UnitTests/TankDriveMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UnitTests/TankDriveMovement.java
@@ -6,6 +6,7 @@ public class TankDriveMovement extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-
+        //first turn to the angle
+        // then move to the position.
     }
 }
