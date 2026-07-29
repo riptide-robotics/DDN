a865bbb6b5b6138e54ca820f7e5b9cb1052fdd54
sarfie <110944623+sarfie@users.noreply.github.com>
2026-07-29 14:51:42 -0700

    work on OrderedSequence

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sequencer/OrderedSequence.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sequencer/OrderedSequence.java
@@ -1,8 +1,27 @@
 package org.firstinspires.ftc.teamcode.Sequencer;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class OrderedSequence extends SequenceBase{
-    public OrderedSequence(Runnable runnable) {
-        super(runnable);
+    LinkedList<SequenceBase> sequences = new LinkedList<>();
+    Stream<SequenceBase> sequenceBaseStream = sequences.stream();
+    int stage = 0;
+    public OrderedSequence() {
+        super(() -> {}); //has to be edited after constructed lol
+
+        this.runnable = () -> {
+            Iterator<SequenceBase> iterate = sequenceBaseStream.iterator();
+            while (iterate.hasNext()) {
+                SequenceBase base =  iterate.next();
+                if (!(base.canExecute = base.iterationLoop())) break;
+                base.runnable.run();
+
+            }
+        };
     }
 
     @Override
