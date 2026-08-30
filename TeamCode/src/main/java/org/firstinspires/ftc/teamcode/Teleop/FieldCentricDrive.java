e17f03609cbe93be4787bef3c61dbecf10785fa1
MrWalium <coolcoderai123@gmail.com>
2026-08-29 19:44:52 -0700

    unique message 6

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/FieldCentricDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/FieldCentricDrive.java
@@ -1,12 +1,14 @@
 package org.firstinspires.ftc.teamcode.Teleop;
 
+// READY TO TRANSFER
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.Robot;
 
-@TeleOp(name = "AAAaaAAAAaaaAA")
+@TeleOp(name = "Field Centric Drive", group = "Basic Tests")
 public class FieldCentricDrive extends LinearOpMode {
 
     public Robot robot;
