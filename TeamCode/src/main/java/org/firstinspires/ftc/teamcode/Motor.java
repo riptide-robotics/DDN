bb647d2db503ea43c4556d285ca97c31143be828
Joseph Berberet OG <josephberberet@gmail.com>
2026-09-13 19:52:51 -0700

    adding motor test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motor.java
@@ -1,4 +1,43 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Motor {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import java.util.Map;
+import java.util.Objects;
+
+@TeleOp (name = "name1")
+public class Motor extends LinearOpMode {
+    DcMotor motor;
+    Servo servo;
+
+    @Override
+    public void runOpMode(){
+
+        telemetry.addData("Motors", hardwareMap.dcMotor.entrySet());
+        telemetry.update();
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+
+        for (Map.Entry<String, DcMotor> entry: hardwareMap.dcMotor.entrySet()) {
+            if (Objects.equals(entry.getKey(), "motor")) motor = entry.getValue();
+        }
+        for (Map.Entry<String, Servo> entry: hardwareMap.servo.entrySet()) {
+            if (Objects.equals(entry.getKey(), "servo")) servo = entry.getValue();
+        }
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                motor.setPower(1);
+
+            }
+            else {
+                motor.setPower(0);
+            }
+        }
+    }
 }
